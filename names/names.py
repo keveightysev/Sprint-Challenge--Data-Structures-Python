c9148046d226ec1d12455473a1d6d57a93d9fc35
Kevin Smith <keveightysev@gmail.com>
2019-09-16 11:49:22 -0500

    Completed stretch

--- names/names.py
+++ names/names.py
@@ -10,6 +10,8 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# original
+
 duplicates = []
 
 names_set = set(names_1)
@@ -22,3 +24,24 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# stretch
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+stretch_duplicates = []
+
+for name in names_2:
+    if name in names_1:
+        stretch_duplicates.append(name)
+
+end_time = time.time()
+print (f"{len(stretch_duplicates)} duplicates:\n\n{', '.join(stretch_duplicates)}\n\n")
+print (f"Stretch runtime: {end_time - start_time} seconds")
